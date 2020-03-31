2ba52defb12462d852a5d60277170c5024c7ca48
Franklin Diaz <fdiaz@paloaltonetworks.com>
2020-03-31 11:05:14 -0400

    format tf files

--- aws/machines.tf
+++ aws/machines.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "franklin-iac" {
   ami           = "${var.ami}"
   instance_type = "t2.micro"
   key_name      = "${var.key_name}"
+
   # comma separated list of groups
   vpc_security_group_ids      = ["${aws_security_group.iac_sec_grp.id}"]
   subnet_id                   = "${aws_subnet.franklin-iac.id}"
@@ -12,16 +13,16 @@ resource "aws_instance" "franklin-iac" {
     volume_type           = "standard"
     delete_on_termination = "true"
   }
-   
+
   tags = {
     Name = "ubuntu-ctf-scoreboard"
   }
 
   connection {
-    user    = "ubuntu"
-  //  timeout = "1m"
-  //  #agent       = false
-  //  private_key = "${file("~/.ssh/id_rsa")}"
-  }
+    user = "ubuntu"
 
+    //  timeout = "1m"
+    //  #agent       = false
+    //  private_key = "${file("~/.ssh/id_rsa")}"
+  }
 }
