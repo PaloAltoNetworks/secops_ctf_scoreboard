ba8236212dee413b3ed87f084da2ed073ea15113
Franklin Diaz <fdiaz@paloaltonetworks.com>
2020-04-30 16:20:36 -0400

    update packer

--- packer/ctfd_setup.sh
+++ packer/ctfd_setup.sh
@@ -11,11 +11,18 @@
 if [ ! -d "/home/ubuntu/CTFd" ]; then
   cd /home/ubuntu && git clone https://github.com/CTFd/CTFd.git
 fi
-# Setuptools v45.0.0 breaks virtualenv on Python 2
-# https://github.com/pypa/virtualenv/issues/1493
-sudo pip install --upgrade 'setuptools<45.0.0'
-cd /home/ubuntu/CTFd && sudo bash prepare.sh
-sudo bash /home/ubuntu/CTFd/scripts/install_docker.sh
-#sudo newgrp docker
-# Make this a cron entry
-#screen -d -m docker run -p 8000:8000 -it ctfd/ctfd
+if pip 2> /dev/null; then
+  sudo DEBIAN_FRONTEND=noninteractive apt install -y python-dev python-pip
+  # Setuptools v45.0.0 breaks virtualenv on Python 2
+  # https://github.com/pypa/virtualenv/issues/1493
+  sudo pip install --upgrade 'setuptools<45.0.0'
+fi
+if pgrep -x "screen" > /dev/null
+then
+  exit 0
+else
+  cd /home/ubuntu/CTFd && sudo bash prepare.sh
+  sudo bash /home/ubuntu/CTFd/scripts/install_docker.sh
+  sudo chmod a+rw /var/run/docker.sock
+  screen -d -m docker run -p 8000:8000 -it ctfd/ctfd
+fi
