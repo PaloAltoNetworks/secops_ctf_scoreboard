d33617fb65c661646da6e50439bd43fafbf8b359
FrankTheTank <fdiaz@paloaltonetworks.com>
2020-04-30 16:31:11 -0700

    update packer (#29)
    * update packer
    
    * update ctfd service

--- packer/ctfd_setup.sh
+++ packer/ctfd_setup.sh
@@ -8,14 +8,26 @@
 #       CREATED: 03/31/2020 16:42
 #      REVISION:  0.1
 #============================================================
+#!/bin/bash - 
 if [ ! -d "/home/ubuntu/CTFd" ]; then
   cd /home/ubuntu && git clone https://github.com/CTFd/CTFd.git
+  cd /home/ubuntu/CTFd && sudo bash prepare.sh
+  sudo bash /home/ubuntu/CTFd/scripts/install_docker.sh
+  sudo chmod a+rw /var/run/docker.sock
 fi
-# Setuptools v45.0.0 breaks virtualenv on Python 2
-# https://github.com/pypa/virtualenv/issues/1493
-sudo pip install --upgrade 'setuptools<45.0.0'
-cd /home/ubuntu/CTFd && sudo bash prepare.sh
-sudo bash /home/ubuntu/CTFd/scripts/install_docker.sh
-#sudo newgrp docker
-# Make this a cron entry
-#screen -d -m docker run -p 8000:8000 -it ctfd/ctfd
+if [[ $(pip) ]]; then
+  echo "found pip"
+else
+  sudo DEBIAN_FRONTEND=noninteractive apt install -y python-dev python-pip
+  # Setuptools v45.0.0 breaks virtualenv on Python 2
+  # https://github.com/pypa/virtualenv/issues/1493
+  sudo pip install --upgrade 'setuptools<45.0.0'
+fi
+if [[ $(docker ps | grep -v COMMAND) ]]; 
+then
+  echo "docker running"
+  exit 0
+else
+  echo "starting docker"
+  docker run -d --rm -p 8000:8000 -it ctfd/ctfd
+fi
